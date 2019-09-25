45bd5dc3627c7f29a150280846a1e793aa61d17e
Sujohn78 <stonebo0sh56@gmail.com>
2019-09-25 19:41:50 +0300

    Updates

---
+++ precache-manifest.4003e0a5ae8f6b4bcb1612b49fad9996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b049408b67584b7b0028755c58b5eb0f",
+    "url": "/shop-test/index.html"
+  },
+  {
+    "revision": "3ae4fdfb255debbc2b57",
+    "url": "/shop-test/static/css/main.4ee564ec.chunk.css"
+  },
+  {
+    "revision": "904fcbf43a5585372f1e",
+    "url": "/shop-test/static/js/2.f1aa7220.chunk.js"
+  },
+  {
+    "revision": "3ae4fdfb255debbc2b57",
+    "url": "/shop-test/static/js/main.45970028.chunk.js"
+  },
+  {
+    "revision": "c24f57e69089b889823a",
+    "url": "/shop-test/static/js/runtime-main.08da9204.js"
+  }
+]);
